99a2f024aa96fb5530260fc04f5a6c504d05b5f0
Dominik Raducki <dominik.raducki@smartbees.co>
2024-04-27 18:45:00 +0200

    master. Custom context menu - added ability to hide menu.

--- src/content/useCustomContextMenu.ts
+++ src/content/useCustomContextMenu.ts
@@ -1,59 +1,66 @@
 interface Position {
-  left: number;
-  top: number;
+    left: number;
+    top: number;
 }
 
 export const useCustomContextMenu = (
-  target: HTMLDivElement,
-  position: Position
+    target: HTMLDivElement,
+    position: Position
 ) => {
-  const CONTEXT_MENU_CLASS = "custom-context-menu";
+    const CONTEXT_MENU_CLASS = "custom-context-menu";
+    let contextMenu: HTMLDivElement | null = null;
 
-  const createContextBox = () => {
-    const div = document.createElement("div");
-    div.classList.add(CONTEXT_MENU_CLASS);
+    const createContextBox = () => {
+        const div = document.createElement("div");
+        div.classList.add(CONTEXT_MENU_CLASS);
 
-    div.innerHTML = `
+        div.innerHTML = `
     <p>Siemka</p>
     `;
 
-    div.style.left = `${position.left}px`;
-    div.style.top = `${position.top}px`;
+        div.style.left = `${position.left}px`;
+        div.style.top = `${position.top}px`;
 
-    document.body.appendChild(div);
-  };
+        document.body.appendChild(div);
 
-  const removeScroll = () => {
-    document.body.style.overflow = "hidden";
-  };
+        contextMenu = div;
+    };
 
-  const closeContextMenu = () => {
-    console.log("TERAz");
-  };
+    const toggleBodyScroll = () => {
+        document.body.classList.toggle("hide-scroll");
+    };
 
-  const handleOutsideClick = () => {
-    document.body.addEventListener("mousedown", (ev) => {
-      const target = ev.target as HTMLDivElement;
-      if (target.classList.contains(CONTEXT_MENU_CLASS)) return;
+    const closeContextMenu = () => {
+        if (!contextMenu) return;
+        
+        toggleBodyScroll();
+        contextMenu.remove();
+        target.classList.remove("picker-active");
+    };
 
-      const isOutsideClick = !target.closest(".custom-context-menu");
+    const handleOutsideClick = () => {
+        document.body.addEventListener("mousedown", (ev) => {
+            const target = ev.target as HTMLDivElement;
+            if (target.classList.contains(CONTEXT_MENU_CLASS)) return;
 
-      if (!isOutsideClick) return;
+            const isOutsideClick = !target.closest(".custom-context-menu");
 
-      closeContextMenu();
-    });
-  };
+            if (!isOutsideClick) return;
 
-  const handleEscapeClick = () => {
-    document.addEventListener("keydown", (ev) => {
-      if (ev.key === "Escape") {
-        closeContextMenu();
-      }
-    });
-  };
+            closeContextMenu();
+        });
+    };
 
-  createContextBox();
-  removeScroll();
-  handleOutsideClick();
-  handleEscapeClick();
+    const handleEscapeClick = () => {
+        document.addEventListener("keydown", (ev) => {
+            if (ev.key === "Escape") {
+                closeContextMenu();
+            }
+        });
+    };
+
+    createContextBox();
+    toggleBodyScroll();
+    handleOutsideClick();
+    handleEscapeClick();
 };
